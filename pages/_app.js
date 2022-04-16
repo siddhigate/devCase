60cadda4680c12a62ea24b47b3795fe255336177
kedaroo <krb1202@gmail.com>
2022-04-16 14:06:48 +0530

    wrapped app in context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import { UserContextProvider } from '../src/context/user-context'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  
-  return <Component {...pageProps} />
+
+  return (
+    <UserContextProvider>
+      <Component {...pageProps} />
+    </UserContextProvider>
+  )
 }
 
 export default MyApp
