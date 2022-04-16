9c057454a924a40f2337cf5755d8aa58eebc2344
kedaroo <krb1202@gmail.com>
2022-04-16 14:11:54 +0530

    feat: onboarding ui

--- src/components/Onboarding.js
+++ src/components/Onboarding.js
@@ -1,7 +1,30 @@
+import { useState } from 'react'
+
 export default function Onboarding() {
+
+    const [username, setUsername] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(username)
+    }
+
     return (
         <div>
-            <h1>hello</h1>
+            <h1>hey there 👋</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>enter your github profile name to continue:</span>
+                    <input 
+                        type='text'
+                        placeholder="github profile name"
+                        onChange={e => setUsername(e.target.value)}
+                        value={username}
+                    />
+                </label>
+                <button>go!</button>
+            </form>
+            
         </div>
     )    
 }
