9b2f0391fd578073049f65a3333f7bc0c83d10ae
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-04-17 21:54:36 +0530

    feat: landing page projects markdown

--- pages/preview.js
+++ pages/preview.js
@@ -1,91 +1,153 @@
-import { useUserContext } from "../src/context/user-context"
-import { useEffect, useState } from "react"
+import { useUserContext } from "../src/context/user-context";
+import { useEffect, useState } from "react";
+
+const generateLandingMarkup = (user) => {
+  let landingMarkup = "";
+  if (user && user.github) {
+    landingMarkup += `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Siddhi Gate</title>
+        
+            
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                rel="stylesheet">
+        
+                <link rel="stylesheet" href="https://devcaseui.vercel.app/style.css">
+        
+        </head>
+        <body>
+            
+            <header class="header">
+                <div class="flex-all-center">
+                    <img class="logo" src="${user.github.avatar_url}" alt="profile picture">
+                    ${user.github.login}
+                </div>
+            </header>
+        
+            <main class="main-wrapper">
+                <div class="main-info">
+                    <div class="wrapper">
+                        
+                    <h1>${user.github.name}</h1>
+                    <p>${user.github.bio}</p>
+                    <a href="${user.github.html_url}">Get in touch</a>
+                    </div>
+                </div>
+                <div class="main-image">
+                    <img src="${user.github.avatar_url}" alt="profile picture">
+                </div>
+            </main>
+        </body>
+        </html>`;
+  }
+  return landingMarkup;
+};
+
+const generateProjectsMarkup = (repos) => {
+  let projectMarkup = "";
+
+  if (repos.length > 0) {
+    projectMarkup += `<div className='articles-section'><h2>My Projects 👩‍💻</h2>
+        <div className='projects'>
+            `;
+
+    for (let i = 0; i < 6; i++) {
+      projectMarkup += `
+      
+      <div className="card project-card">
+        <h3>${repos[i].name}</h3>
+        <p>${repos[i].description}</p>
+        <p className="createdAt">Created at: ${new Date(
+          repos[i].createdAt
+        ).toDateString()}</p>
+        <p>Language: ${repos[i].language}</p>
+        
+        <a className="link link-primary" href=${
+          repos[i].liveURL
+        }>live demo</a>
+        <a className="link link-secondary" href=${
+          repos[i].repoURL
+        }>see repo</a>
+    </div>`;
+    }
+
+    projectMarkup += `<div></div`;
+  }
+  return projectMarkup
+};
+
+const generateArticlesMarkup = (articles) => {};
+
+const generateTweetsMarkup = (tweets) => {};
+
+const generateFooterMarkup = (user) => {};
 
 const generateBody = (user) => {
-    const body = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Siddhi Gate</title>
-    
-        
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-            rel="stylesheet">
-    
-            <link rel="stylesheet" href="./style.css">
-    
-    </head>
-    <body>
-        
-        <header class="header">
-            <div class="flex-all-center">
-                <img class="logo" src="${user.github.avatar_url}" alt="profile picture">
-                ${user.github.login}
-            </div>
-        </header>
-    
-        <main class="main-wrapper">
-            <div class="main-info">
-                <div class="wrapper">
-                    
-                <h1>${user.github.name}</h1>
-                <p>${user.github.bio}</p>
-                <a href="${user.github.html_url}">Get in touch</a>
-                </div>
-            </div>
-            <div class="main-image">
-                <img src="${user.github.avatar_url}" alt="profile picture">
-            </div>
-        </main>
-    </body>
-    </html>`
+  let markup = "";
 
-    return body
-}
+  if (!user.github) {
+    return markup;
+  }
+
+  if (user.github) {
+    markup += generateLandingMarkup(user);
+
+    if (user.githubRepos && user.githubRepos.length > 0) {
+      markup += generateProjectsMarkup(user.githubRepos);
+    }
+  }
+  return markup;
+};
 
 export default function Preview() {
+  const [userContent, setUserContent] = useState(null);
+  const { user } = useUserContext();
 
-    const [userContent, setUserContent] = useState(null)
-    const { user } = useUserContext()
+  useEffect(() => {
+    setUserContent(generateBody(user));
+  }, [user]);
 
-    useEffect(() => {
-        setUserContent(generateBody(user))        
-    }, [user])
+  const handleClipboard = () => {
+    navigator.clipboard.writeText(userContent);
+  };
 
-    const handleClipboard = () => {
-        navigator.clipboard.writeText(userContent)
-    }
+  const download = (filename, textInput) => {
+    let element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8, " + encodeURIComponent(textInput)
+    );
+    element.setAttribute("download", filename);
+    document.body.appendChild(element);
+    element.click();
+  };
 
-    const download = (filename, textInput) => {
-        let element = document.createElement('a');
-        element.setAttribute('href','data:text/plain;charset=utf-8, ' + encodeURIComponent(textInput));
-        element.setAttribute('download', filename);
-        document.body.appendChild(element);
-        element.click();
-    }
+  const handleDownload = () => {
+    var filename = "portfolio.html";
+    download(filename, userContent);
+  };
 
-    const handleDownload = () => {
-        var filename = "portfolio.html";
-        download(filename, userContent);
-    }
-
-    return (
-        <>
-            {user && (
-                <div className="preview">
-                    <h1>preview✨</h1>
-                    <button className="btn-preview" onClick={handleClipboard} >copy to clipboard</button>
-                    <button className="btn-preview" onClick={handleDownload} >download</button>
-                    <textarea 
-                        value={userContent}
-                    />
-                </div>
-            )}
-        </>
-    )
+  return (
+    <>
+      {user && (
+        <div className="preview">
+          <h1>preview✨</h1>
+          <button className="btn-preview" onClick={handleClipboard}>
+            copy to clipboard
+          </button>
+          <button className="btn-preview" onClick={handleDownload}>
+            download
+          </button>
+          <textarea value={userContent} />
+        </div>
+      )}
+    </>
+  );
 }
