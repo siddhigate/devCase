b36b5b6972d403ecc58e0f224e2ca70ac0773068
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-04-17 09:29:11 +0530

    fix: user null issue

--- pages/edit.js
+++ pages/edit.js
@@ -1,26 +1,23 @@
- 
-import { useEffect } from 'react';
-import Landing from '../src/components/portfolio/Landing';
-import { useUserContext } from '../src/context/user-context'
-import useGithub from '../src/hooks/useGitHub';
+import { useEffect } from "react";
+import Landing from "../src/components/portfolio/Landing";
+import { useUserContext } from "../src/context/user-context";
+import useGithub from "../src/hooks/useGitHub";
+import { useRouter } from "next/router";
 
 export default function Portfolio() {
+  let { user } = useUserContext();
 
-    let {user} = useUserContext();
+  const { getRepos } = useGithub();
 
-    const {getRepos} = useGithub()
-    
-    useEffect(() => {
+  const router = useRouter();
 
-        if(user.github) {
-            getRepos(user.github.login);
-        }
-    }, [])
-    
+  useEffect(() => {
+    if (user.github) {
+      getRepos(user.github.login);
+    } else {
+      router.push("/");
+    }
+  }, []);
 
-    return (
-        <>
-            <Landing user={user}/>
-        </>
-    )
+  return <>{user && user.github && <Landing user={user} />}</>;
 }
